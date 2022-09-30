d62794c40e8f06c4ff6847b02f77f9a40bd76b14
Julien Schueller <schueller@phimeca.com>
2022-09-30 16:41:27 +0200

    Install shared libs last (#23)
    * Install shared libs last
    
    Else static libs are picked for cmake builds
    
    * MNT: Re-rendered with conda-build 3.22.0, conda-smithy 3.21.2, and conda-forge-pinning 2022.09.28.16.00.31
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Build shared lib
+# Build static lib
 cmake ${CMAKE_ARGS} . \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
     -DBUILD_TESTING=ON
-make -j
+make install -j${CPU_COUNT}
 
-test/cctest/cctest --list | tr -d '<' | xargs test/cctest/cctest
-
-make install
-
-# Build static lib
+# Build shared lib
 cmake . \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_SHARED_LIBS=ON \
   -DBUILD_TESTING=OFF
-make -j
-make install
+make install -j${CPU_COUNT}
+
+test/cctest/cctest --list | tr -d '<' | xargs test/cctest/cctest
+
